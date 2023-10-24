8eab4c4ba402e93cd0d010cf5c49b9d56af49837
Walter Sosa <wasosa@yahoo.com>
2023-10-23 20:27:56 -0700

    Second example: generating table from JSON

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// Called by clicking "Load" button
+///////////////////////////
+// LOADING A FILE FROM DISK
+///////////////////////////
+
+// Called by clicking "Load File" button
 function loadFile()
 {
     console.debug("# loadFile()");
@@ -31,16 +35,93 @@ function setFileContents(contents)
     }
 }
 
-// Called by clicking "Clear" button
+// Called by clicking "Clear File" button
 function resetFileContents()
 {
     setFileContents("File contents will appear here.");
+}
+
+// Called by clicking "Clear Table" button
+function resetTableContents()
+{
+    setFileContents("File contents will appear here.");
+}
+
+///////////////////////////////
+// GENERATING A TABLE FROM JSON
+///////////////////////////////
+
+function loadTable()
+{
+    console.debug("# loadTable()");
+    let tableFile = document.getElementById("load-table").files[0];
+    if (tableFile) {
+        console.debug("a table file has been chosen");
+        let reader = new FileReader();
+        reader.readAsText(tableFile, "UTF-8");
+        reader.onload = tableFileLoaded;
+    } else {
+        console.debug("no table file has been chosen");
+    }
+}
+
+function tableFileLoaded(event) {
+    console.debug("# tableFileLoaded()");
+    contents = event.target.result;
+    setTableFileContents(contents);
+}
+
+function setTableFileContents(_contents)
+{
+    console.debug("# setTableFileContents()");
+
+    let table_contents = document.getElementById("table-contents");
+    if (!table_contents) {
+        console.error("could not find element: table-contents");
+        return;
+    }
+
+    let contents = JSON.parse(_contents)
+    let transactions = contents["transactions"] || null;
+    if (!transactions) {
+        return;
+    }
+
+    let table = document.createElement('table');
+    let cols = Object.keys(transactions[0]);
+    let tr = document.createElement('tr');
+    for (let i = 0; i < cols.length; i++) {
+        let th = document.createElement('th');
+        th.appendChild(document.createTextNode(cols[i]));
+        tr.appendChild(th);
+    }
+    table.append(tr);
+
+    for (let i = 0; i < transactions.length; i++) {
+        let element = transactions[i];
+        tr = document.createElement('tr');
+        for (let j = 0; j < cols.length; j++) {
+            let key = cols[j];
+            let td = document.createElement('td');
+            td.appendChild(document.createTextNode(element[key]));
+            tr.append(td);
+        }
+        table.append(tr);
+    }
+    table_contents.append(table);
+}
+
+// Called by clicking "Clear File" button
+function resetTableFileContents()
+{
+    setTableFileContents("{}");
 }
 
 function main()
 {
     console.debug("# main()");
     resetFileContents();
+    resetTableFileContents();
 }
 
 // ---------- start of testing code ----------
